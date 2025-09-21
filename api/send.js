cc248a3c53c80cd82a20e0fc59a918ffd3a8107e
greejjddg09 <127425061+greejjddg09@users.noreply.github.com>
2025-09-21 08:09:13 +0500

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,24 +1,22 @@
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    const { result } = req.body;  // <-- вот здесь сервер получает score
+    const { result, userId } = req.body;
 
     const BOT_TOKEN = process.env.BOT_TOKEN;
-    const CHAT_ID = process.env.CHAT_ID;
-
     const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
 
-    await fetch(url, {
+    const response = await fetch(url, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        chat_id: CHAT_ID,
-        text: `Пользователь прошёл тест! Результат: ${result}/5`
+        chat_id: userId,
+        text: `Ваш результат: ${result}/1`
       })
     });
 
-    res.status(200).json({ ok: true });
+    const data = await response.json();
+    res.status(200).json(data);
   } else {
     res.status(405).end();
   }
 }
-
