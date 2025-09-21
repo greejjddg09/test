f385b47d86627549109146c63547599b4618fa22
greejjddg09 <127425061+greejjddg09@users.noreply.github.com>
2025-09-21 07:51:11 +0500

    Add files via upload

---
+++ notify.js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+    if (req.method !== "POST") {
+      return res.status(405).json({ error: "Method not allowed" });
+    }
+  
+    const { user_id, score } = req.body;
+    const BOT_TOKEN = process.env.BOT_TOKEN; // токен спрятан в настройках Vercel
+  
+    const text = `Ваш результат: ${score}/5 ✅`;
+  
+    await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ chat_id: user_id, text })
+    });
+  
+    res.status(200).json({ ok: true });
+  }
+  
